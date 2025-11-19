a395104881e2bf8a789c8376f7d27ec21ee94d9b
Aniketu28 <uagreaniket28@gmail.com>
2025-11-19 06:36:29 +0530

    created sidebar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Sidebar from '../components/Sidebar'
+import ChatContainer from '../components/ChatContainer'
+import RightSidebar from '../components/RightSidebar'
 
 const HomePage = () => {
+
+    const [SelectedUser, setSelectedUser] = useState(false);
+
     return (
-        <div className='border w-full h-screen sm:px-[15%] sm:py-[5%]'>
-            HomePage
+        <div className='border w-full h-screen flex justify-center items-center sm:px-[15%] sm:py-[5%]'>
+            <div className={`w-[100%] backdrop-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${SelectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
+                <Sidebar SelectedUser={SelectedUser} setSelectedUser={setSelectedUser}/>
+                <ChatContainer/>
+                <RightSidebar/>
+            </div>
         </div>
     )
 }
